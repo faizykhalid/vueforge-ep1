bd84672e50d7edbffa2e9ff5978170b5ba68f258
Faizan Khalid <faizankhalid1636@gmail.com>
2023-04-24 22:07:13 +0500

    added board files

---
+++ src/pages/boards/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+// const boards = ref([
+//   {
+//     title: "My first Board",
+//     order: "[]",
+//     image: {},
+//   },
+//   {
+//     title: "My Second Board",
+//     order: "[]",
+//   },
+// ]);
+</script>
+<template>
+  <h1>Hello Boards</h1>
+</template>
