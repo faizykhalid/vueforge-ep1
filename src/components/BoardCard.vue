1715185ab6949c0473d813278c939baa82c844b7
Faizan Khalid <faizankhalid1636@gmail.com>
2023-04-25 16:41:05 +0500

    added static boards

---
+++ src/components/BoardCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {
+  Card as KCard,
+  CardTitle as KCardTitle,
+} from "@progress/kendo-vue-layout";
+import type { Board } from "@/types";
+import AppImage from "./AppImage.vue";
+defineProps<{
+  board: Board;
+}>();
+const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(10);
+</script>
+<template>
+  <router-link :to="`/boards/${board.id}`" class="block w-96">
+    <KCard class="m-5">
+      <AppImage
+        v-if="board.image"
+        :src="board.image?.downloadUrl"
+        width="384"
+        class="aspect-video w-full"
+      ></AppImage>
+      <div
+        v-else
+        class="aspect-video"
+        :style="{ backgroundColor: randomColor }"
+      ></div>
+      <KCardTitle class="p-2">
+        <span class="text-gray-700 text-xl">{{ board.title }}</span>
+      </KCardTitle>
+    </KCard>
+  </router-link>
+</template>
--- src/pages/boards/index.vue
+++ src/pages/boards/index.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
-// import { ref } from "vue";
-// const boards = ref([
-//   {
-//     title: "My first Board",
-//     order: "[]",
-//     image: {},
-//   },
-//   {
-//     title: "My Second Board",
-//     order: "[]",
-//   },
-// ]);
+import { ref } from "vue";
+import type { Board } from "@/types";
+const boards = ref<Partial<Board>[]>([
+  {
+    id: "1",
+    title: "My first Board",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?boards?=1",
+    },
+  },
+  {
+    id: "2",
+    title: "My Second Board",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?boards?=2",
+    },
+  },
+  {
+    id: "3",
+    title: "My Third Board",
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?boards?=3",
+    },
+  },
+]);
 </script>
 <template>
-  <h1>Hello Boards</h1>
+  <h1 class="text-3xl">Boards</h1>
+  <board-card v-for="board in boards" :key="board.id" :board="board" />
+  <!-- <button class="text-gray-500" @click="createBoard(newBoardTemplate)">
+    <span>New Board +</span>
+  </button> -->
 </template>
